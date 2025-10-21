e3878f1e5170d03cb3079fa749b9563d0e6909d9
Haftu Girmay <72265372+Haftush@users.noreply.github.com>
2025-10-21 15:43:52 +0300

    Fix Telegram link in Contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -32,7 +32,7 @@ export default function Contact({ onNavigate }: ContactProps) {
       icon: <Send className="w-8 h-8" />,
       // title: "Telegram",
       value: "@haphi_luxury",
-      link: "https://t.me/@haphi_luxury",
+      link: "https://t.me/haphi_luxury",
       color: "bg-blue-500",
     },
     {
